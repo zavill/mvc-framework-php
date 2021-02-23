9ce4b010e00510045ebba33b94fd2b50dd3a4268
Daniil Zavilohin <denik6740@gmail.com>
2021-02-23 21:17:08 +0300

    Изменен алгоритм подключения библиотек для реализации точки входа в других директориях

--- test/index.php
+++ test/index.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-require_once '../app/lib/develop.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/app/lib/develop.php';
 
 use app\core\Router;
 
 $router = new Router();
 
-$router->add('test', ['controller' => 'main', 'action' => 'index']);
+$router->add('test/doubleTest', ['controller' => 'main', 'action' => 'index']);
 
 $router->dispatch();
